b930e81fd4a849abbcb5a8f80070e87d00f1e413
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-04-14 19:07:04 +0200

    fix: Try to increase mixin priority to maybe support some other mods that also patch options better #76

--- shared/src/main/java/net/blay09/mods/defaultoptions/mixin/OptionsMixin.java
+++ shared/src/main/java/net/blay09/mods/defaultoptions/mixin/OptionsMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Options.class)
+@Mixin(value = Options.class, priority = 100)
 public class OptionsMixin {
     @Inject(method = "load()V", at = @At("HEAD"))
     private void load(CallbackInfo ci) {
