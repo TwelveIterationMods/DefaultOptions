def56aab7920d37c112814ab4da7563f73be70dd
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-07-22 13:30:12 +0200

    refactor: Remove duplicate code

--- common/src/main/java/net/blay09/mods/defaultoptions/DefaultOptionsInitializer.java
+++ common/src/main/java/net/blay09/mods/defaultoptions/DefaultOptionsInitializer.java
@@ -2,7 +2,10 @@ package net.blay09.mods.defaultoptions;
 
 import net.blay09.mods.balm.api.event.client.ClientStartedEvent;
 import net.blay09.mods.defaultoptions.api.*;
+import net.minecraft.client.Options;
+import net.minecraft.nbt.CompoundTag;
 
+import java.util.HashSet;
 import java.util.ServiceLoader;
 
 public class DefaultOptionsInitializer {
@@ -31,6 +34,21 @@ public class DefaultOptionsInitializer {
                 } catch (DefaultOptionsHandlerException e) {
                     DefaultOptions.logger.error("Failed to load default options for {}", e.getHandlerId(), e);
                 }
+            }
+        }
+    }
+
+    public static void detectAndMarkModifiedKeys(Options options, CompoundTag fields) {
+        final var knownKeys = new HashSet<String>();
+        for (final var option : fields.getAllKeys()) {
+            if (option.startsWith("key_")) {
+                final var name = option.substring("key_".length());
+                knownKeys.add(name);
+            }
+        }
+        for (final var keyMapping : options.keyMappings) {
+            if (knownKeys.contains(keyMapping.getName())) {
+                ((DefaultOptionsKeyMapping) keyMapping).defaultoptions$setUserModified(true);
             }
         }
     }
--- fabric/src/main/java/net/blay09/mods/defaultoptions/mixin/FabricOptionsMixin.java
+++ fabric/src/main/java/net/blay09/mods/defaultoptions/mixin/FabricOptionsMixin.java
@@ -1,13 +1,11 @@
 package net.blay09.mods.defaultoptions.mixin;
 
-import net.blay09.mods.defaultoptions.DefaultOptionsKeyMapping;
+import net.blay09.mods.defaultoptions.DefaultOptionsInitializer;
 import net.minecraft.client.Options;
 import net.minecraft.nbt.CompoundTag;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-
-import java.util.HashSet;
 
 @Mixin(Options.class)
 public class FabricOptionsMixin {
@@ -16,19 +14,7 @@ public class FabricOptionsMixin {
         // This will operate on raw options data before data fixers are applied to it, but I think that's fine
         // considering key mapping options have been stable for a long time and even if they ever change,
         // it would be extremely rare to happen in a modpack environment.
-        final var options = (Options) (Object) this;
-        final var knownKeys = new HashSet<String>();
-        for (final var option : fields.getAllKeys()) {
-            if (option.startsWith("key_")) {
-                final var name = option.substring("key_".length());
-                knownKeys.add(name);
-            }
-        }
-        for (final var keyMapping : options.keyMappings) {
-            if (knownKeys.contains(keyMapping.getName())) {
-                ((DefaultOptionsKeyMapping) keyMapping).defaultoptions$setUserModified(true);
-            }
-        }
+        DefaultOptionsInitializer.detectAndMarkModifiedKeys((Options) (Object) this, fields);
         return fields;
     }
 
