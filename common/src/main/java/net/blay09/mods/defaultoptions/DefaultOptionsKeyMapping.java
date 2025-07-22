32399604ad6bb6fc7310bf660c47594502e088cd
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-07-22 12:51:38 +0200

    fix: Track known keys as they are loaded, as setKey is not called when it matches the original default

--- common/src/main/java/net/blay09/mods/defaultoptions/DefaultOptionsKeyMapping.java
+++ common/src/main/java/net/blay09/mods/defaultoptions/DefaultOptionsKeyMapping.java
@@ -2,4 +2,5 @@ package net.blay09.mods.defaultoptions;
 
 public interface DefaultOptionsKeyMapping {
     boolean defaultoptions$wasUserModified();
+    void defaultoptions$setUserModified(boolean modified);
 }
---
+++ forge/src/main/java/net/blay09/mods/defaultoptions/forge/mixin/ForgeKeyMappingMixin.java
@@ -0,0 +1,22 @@
+package net.blay09.mods.defaultoptions.forge.mixin;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.blay09.mods.defaultoptions.DefaultOptionsKeyMapping;
+import net.minecraft.client.KeyMapping;
+import net.minecraftforge.client.settings.KeyModifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(KeyMapping.class)
+public class ForgeKeyMappingMixin {
+
+    @Inject(method = "setKeyModifierAndCode", at = @At("HEAD"), remap = false)
+    void setKeyModifierAndCode(KeyModifier keyModifier, InputConstants.Key keyCode, CallbackInfo ci) {
+        // setKey is only called when the key didn't match the default on options load, so it's not reliable.
+        // We just track it additionally to cover all bases.
+        ((DefaultOptionsKeyMapping) this).defaultoptions$setUserModified(true);
+    }
+
+}
