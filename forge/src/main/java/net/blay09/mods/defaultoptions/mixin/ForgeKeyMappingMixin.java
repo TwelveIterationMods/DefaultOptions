5749119ebe844632d6671835f96d437bb1a49f34
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-07-22 13:03:56 +0200

    fix: Fix Forge mixin

--- forge/src/main/java/net/blay09/mods/defaultoptions/mixin/ForgeKeyMappingMixin.java
+++ forge/src/main/java/net/blay09/mods/defaultoptions/mixin/ForgeKeyMappingMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ForgeKeyMappingMixin {
 
     @Inject(method = "setKeyModifierAndCode", at = @At("HEAD"), remap = false)
-    void setKeyModifierAndCode(KeyModifier keyModifier, InputConstants.Key keyCode) {
+    void setKeyModifierAndCode(KeyModifier keyModifier, InputConstants.Key keyCode, CallbackInfo ci) {
         // setKey is only called when the key didn't match the default on options load, so it's not reliable.
         // We just track it additionally to cover all bases.
         ((DefaultOptionsKeyMapping) this).defaultoptions$setUserModified(true);
