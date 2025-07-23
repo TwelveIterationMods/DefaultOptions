1d2934ac07b61aa77710eb5cb2a81b85934ee5fc
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-07-23 09:19:13 +0200

    feat: Add some more log lines

--- common/src/main/java/net/blay09/mods/defaultoptions/mixin/OptionsMixin.java
+++ common/src/main/java/net/blay09/mods/defaultoptions/mixin/OptionsMixin.java
@@ -11,8 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class OptionsMixin {
     @Inject(method = "load()V", at = @At("HEAD"))
     private void load(CallbackInfo ci) {
-        DefaultOptionsInitializer.collectSeenKeys((Options) (Object) this);
-        DefaultOptionsInitializer.preLoad();
+        DefaultOptionsInitializer.preLoad((Options) (Object) this);
     }
 
     @Inject(method = "save()V", at = @At("RETURN"))
