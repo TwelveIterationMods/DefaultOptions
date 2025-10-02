007940b69b3077435f508284200633d1481a61a8
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-10-02 16:04:20 +0200

    fix: Apply extra options as early as possible, before Fabric entrypoints run

---
+++ common/src/main/java/net/blay09/mods/defaultoptions/mixin/MinecraftMixin.java
@@ -0,0 +1,25 @@
+package net.blay09.mods.defaultoptions.mixin;
+
+import net.blay09.mods.defaultoptions.DefaultOptionsInitializer;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = Minecraft.class, priority = 100)
+public class MinecraftMixin {
+    @Inject(
+            method = "<init>",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/client/Minecraft;gameDirectory:Ljava/io/File;",
+                    opcode = Opcodes.PUTFIELD,
+                    shift = At.Shift.AFTER
+            )
+    )
+    private void init(CallbackInfo ci) {
+        DefaultOptionsInitializer.earlyInit();
+    }
+}
