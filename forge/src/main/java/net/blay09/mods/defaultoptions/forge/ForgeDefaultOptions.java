90a9c358c23c5c70c6cb00e36d357dfe9e7e02c3
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-12-15 13:08:58 +0100

    feat: Update to Forge 1.21.11

--- forge/src/main/java/net/blay09/mods/defaultoptions/forge/ForgeDefaultOptions.java
+++ forge/src/main/java/net/blay09/mods/defaultoptions/forge/ForgeDefaultOptions.java
@@ -2,8 +2,8 @@ package net.blay09.mods.defaultoptions.forge;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.client.BalmClient;
-import net.blay09.mods.balm.forge.ForgeLoadContext;
+import net.blay09.mods.balm.client.BalmClient;
+import net.blay09.mods.balm.forge.platform.runtime.ForgeLoadContext;
 import net.blay09.mods.defaultoptions.DefaultOptions;
 import net.blay09.mods.defaultoptions.PlatformBindings;
 import net.blay09.mods.defaultoptions.forge.mixin.ForgeKeyMappingAccessor;
